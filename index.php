8303b7bc0e4ca775f82b5112a53887a718d4643c
lightningexperience <lightningexperience@users.noreply.github.com>
2018-01-05 13:38:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ Freely reusable with no restrictions
 // You need to set these three to the values for your own application
 define('CONSUMER_KEY', '3MVG9xOCXq4ID1uE0MRhgm7ZNA._jS3YgFoIDPY2HFqaNKftS8LJovQ6ro0E3b.yXQmELANJDnxfZhlSvqSEK');
 define('CONSUMER_SECRET', '8483951908878986038');
-define('REDIRECT_URI', 'https://phprest001.herokuapp.com/');
+define('REDIRECT_URI', '');
 
 define('LOGIN_BASE_URL', 'https://login.salesforce.com');
 
